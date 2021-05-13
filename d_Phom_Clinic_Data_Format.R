3db33d3c34a943b87079fcc78860f4b735042ed0
Raoul Wadhwa <raoulwadhwa@gmail.com>
2021-05-13 11:33:52 -0400

    minor stylistic changes

--- d_Phom_Clinic_Data_Format.R
+++ d_Phom_Clinic_Data_Format.R
@@ -28,82 +28,100 @@ list.raw.phom <- readRDS("list.raw.phom.rds")
 list.all.data <- list()
 
 #First adding IDs
-for (i in 1:length(list.raw.phom)) {
-  Patient.ID <- str_split(names(list.raw.phom[i]), "_", n = 2)[[1]][1]
-  Scan.ID <- str_split(names(list.raw.phom[i]), "_", n = 2)[[1]][2]
+for (i in seq_len(length(list.raw.phom))) {
+  get_ID <- list.raw.phom[i] %>%
+    names %>%
+    strsplit(split = "_", n = 2) %>%
+    unlist
   
-  list.all.data[[i]] <- c(Patient.ID = Patient.ID, Scan.ID = Scan.ID)
+  list.all.data[[i]] <- c(Patient.ID = get_ID[1],
+                          Scan.ID = get_ID[2])
 }
 
 #Function to count total features that exist at each threshold value
-cubical.total.feat.counter <- function(phom, min, max, res) {
-  by <- (max-min)/res
-  seq.to.check <- seq(min, max, by)
-  mat <- cbind(rep(0, length(seq.to.check)), seq.to.check)
-  for (i in 1:length(seq.to.check)) {
+cubical.total.feat.counter <- function(phom, minimum, maximum, res) {
+  by <- (maximum - minimum) / res
+  seq.to.check <- seq(minimum, maximum, by)
+  
+  mat <- seq.to.check %>%
+    length %>%
+    rep(x = 0) %>%
+    cbind(seq.to.check)
+  
+  for (i in seq_len(length(seq.to.check))) {
     tally <- sum(seq.to.check[i] >= phom[,2] & 
-                   seq.to.check[i] <= phom[,3])
+                 seq.to.check[i] <= phom[,3])
     
     mat[i, 1] <- tally
   }
-  return(mat[,c(2,1)])
+  
+  return(mat[, c(2, 1)])
 }
 
 #Creating a function to count based on individual dim feat
-featcounter.vector.cubical.act <- function(phom, min, max, res, feat) {
+featcounter.vector.cubical.act <- function(phom, minimum, maximum, res, feat) {
   
-  phom <- phom[which(phom[,1] == feat),]
+  phom <- phom[which(phom[, 1] == feat), ]
   
-  by <- (max-min)/res
-  seq.to.check <- seq(min, max, by)
-  mat <- cbind(rep(0, length(seq.to.check)), seq.to.check)
-  for (i in 1:length(seq.to.check)) {
-    tally <- sum(seq.to.check[i] >= phom[,2] & 
-                   seq.to.check[i] <= phom[,3])
+  by <- (maximum - minimum) / res
+  seq.to.check <- seq(minimum, maximum, by)
+  
+  mat <- seq.to.check %>%
+    length %>%
+    rep(x = 0) %>%
+    cbind(seq.to.check)
+  
+  for (i in seq_len(length(seq.to.check))) {
+    tally <- sum(seq.to.check[i] >= phom[, 2] & 
+                 seq.to.check[i] <= phom[, 3])
     
     mat[i, 1] <- tally
   }
+  
   return(mat[,c(2,1)])
 }
 
-#Adding TDA Data 
-for (i in 1:length(list.raw.phom)) {
-#Removing the -1 dimension and the death at 9999
-phom.test.crr <- list.raw.phom[[i]][-which(list.raw.phom[[i]]["dimension"] == -1), ]
-
-#Creating the Topological Feature Curves
-test.0 <- featcounter.vector.cubical.act(phom.test.crr, min = -1024, max = 3071, res = 1000, feat = 0)
-test.1 <- featcounter.vector.cubical.act(phom.test.crr, min = -1024, max = 3071, res = 1000, feat = 1)
-test.2 <- featcounter.vector.cubical.act(phom.test.crr, min = -1024, max = 3071, res = 1000, feat = 2)
-test.tot <- cubical.total.feat.counter(phom.test.crr, min = -1024, max = 3071, res = 1000)
-
-all.feat.curves <- cbind(test.0[,1],
-                         test.0[,2],
-                         test.1[,2],
-                         test.2[,2],
-                         test.tot[,2])
-
-all.feat.curves <- as.data.frame(all.feat.curves)
-
-colnames(all.feat.curves) <- c("HU", "dim0", "dim1", "dim2", "dimtot")
-
-
-
-list.all.data[[i]] <- c(list.all.data[[i]], feat_curves = list(all.feat.curves))
-list.all.data[[i]] <- c(list.all.data[[i]], raw_phom = list(list.raw.phom[[i]])) 
+#Adding TDA Data
+for (i in seq_len(length(list.raw.phom))) {
+  #Removing the -1 dimension and the death at 9999
+  phom.test.crr <- list.raw.phom[[i]][-which(list.raw.phom[[i]]["dimension"] == -1), ]
+  
+  #Creating the Topological Feature Curves
+  test.0 <- featcounter.vector.cubical.act(phom.test.crr, min = -1024, max = 3071,
+                                                          res = 1000, feat = 0)
+  test.1 <- featcounter.vector.cubical.act(phom.test.crr, min = -1024, max = 3071,
+                                                          res = 1000, feat = 1)
+  test.2 <- featcounter.vector.cubical.act(phom.test.crr, min = -1024, max = 3071,
+                                                          res = 1000, feat = 2)
+  test.tot <- cubical.total.feat.counter(phom.test.crr, min = -1024, max = 3071,
+                                                        res = 1000)
+  
+  all.feat.curves <- cbind(test.0[, 1],
+                           test.0[, 2],
+                           test.1[, 2],
+                           test.2[, 2],
+                           test.tot[, 2])
+  
+  all.feat.curves <- as.data.frame(all.feat.curves)
+  
+  colnames(all.feat.curves) <- c("HU", "dim0", "dim1", "dim2", "dimtot")
+  
+  list.all.data[[i]] <- c(list.all.data[[i]], feat_curves = list(all.feat.curves))
+  list.all.data[[i]] <- c(list.all.data[[i]], raw_phom = list(list.raw.phom[[i]])) 
 }
 
 #Visualizing Random Curves
-topfeatcurv.melt <- melt(list.all.data[[167]][["feat_curves"]], id.vars = c("HU"), 
+topfeatcurv.melt <- melt(list.all.data[[167]][["feat_curves"]],
+                         id.vars = c("HU"), 
                          measure.vars = c("dim0", "dim1", "dim2", "dimtot"))
-
 colnames(topfeatcurv.melt) <- c("filtration", "feature.type", "feature.count")
 
-
 ggplot(topfeatcurv.melt, aes(x = filtration, y = feature.count, color = feature.type)) +
-  geom_path(size = 1) + theme_bw() +
+  geom_path(size = 1) +
+  theme_bw() +
   labs(x = "Filtration (HU)", 
-       y = "Number of Features", color = "Feature Dimension") 
+       y = "Number of Features",
+       color = "Feature Dimension") 
 
 #Clinic Data Retrieve
 key <- read_excel("/Volumes/Cancr/physics/Peng/Lung_SBRT/Radiomics/Key.xlsx")
@@ -111,7 +129,7 @@ clinical.data <- read_excel("clinic_data.xlsx")
 date_IDs <- readRDS("Scan_Date_ID_Key.rds")
 
 #Incorporate Clinical Variables
-for (i in 1:length(list.all.data)) {
+for (i in seq_len(length(list.all.data))) {
 
   #Getting the CCF MRN Number
   pid <- list.all.data[[i]]$Patient.ID
@@ -129,85 +147,85 @@ for (i in 1:length(list.all.data)) {
   #Only getting data if an MRN is present
   if(nrow(clinic.MRN) == 1) {
   
-  #Getting the clinical Data into a data frame
-  clin.dat <- cbind.data.frame(
+    #Getting the clinical Data into a data frame
+    clin.dat <- cbind.data.frame(
     #Basic initial ID Information (MRN, Scan Date, Sex, Ethnicity, DOB, Age, Diagnosis Date, Rad Start Date)
-    MRN = MRN, 
-    Scan.Date = scan.date, 
-    Sex = clinic.MRN$Gender, 
-    Ethnicity = clinic.MRN$Ethnicity, 
-    DOB = clinic.MRN$`Date of Birth`, 
-    Age.Diag = clinic.MRN$`Age at Diagnosis`, 
-    DODiag = clinic.MRN$`Date of Diagnosis...170`, 
-    Date.Rad.Start = clinic.MRN$`RT Start Date...199`,
-    
-    #Basic Patient Health Information (BMI, Years SMoking, KPS, HGB, CCI comorbidity, 
-    #Previous Cancer Diagnosis, #Number of Previous Cancer Diagnoses
-    BMI = clinic.MRN$`BMI`, 
-    Years.Smoke = clinic.MRN$`Pack Years Smoking`,
-    KPS = clinic.MRN$`KPS`, 
-    HGB = clinic.MRN$`HGB Pre`, 
-    CCI = clinic.MRN$`Total points:`,
-    Prev.Canc.Diag = clinic.MRN$`Previous Cancer Diagnosis?`,
-    Num.Prev.Canc.Diag = clinic.MRN$`Number of Previous Cancer Diagnoses`, 
-    
-    #Treatment Info; prior surgery, pre or post radiation chemo, intent of radiation, dose of radiation
-    Prior.Surg = clinic.MRN$`Prior Surgery?`, 
-    Post.Rad.Chemo = clinic.MRN$`Post-SBRT Chemo/Systemic RX?...657`,
-    Pre.Rad.Chemo = clinic.MRN$`Pre-SBRT Chemo?...191`, 
-    Rad.Intent = clinic.MRN$`Intent...194`, 
-    Tot.Dose = clinic.MRN$`Total Dose (Gy)...201`, 
-    
-    #Cancer Characteristics including tumor size, TNM, overall stage, Histology, Pathology
-    CT.Size = clinic.MRN$`CT (Cm)...177`, 
-    T.Stage = clinic.MRN$`T Stage...179`, 
-    N.Stage = clinic.MRN$`N Stage...180`,
-    M.Stage = clinic.MRN$`M Stage...181`, 
-    Overall.Stage = clinic.MRN$Stage...182, 
-    Histo = clinic.MRN$Histology...189,
-    Path = clinic.MRN$Pathology,
-    
-    #Survival Information
-    Vital.Status = clinic.MRN$`Survival Status`, 
-    OS.Length = as.Date(clinic.MRN$`Date of Death`) - scan.date,
-    Date.Death = clinic.MRN$`Date of Death`,
-    
-    #Progression Information
-    Progression.Status = ifelse(is.na(clinic.MRN$`1st Failure Date...678`), "Stable", "Progressed"), 
-    PFS.Length = as.Date(clinic.MRN$`1st Failure Date...678`) - scan.date,
-    PFS.Date = clinic.MRN$`1st Failure Date...678`,
-    PFS.Failure.Type = clinic.MRN$`Failure Type...677`,
-    
-    #Local Control Information
-    Local.Status = ifelse(clinic.MRN$Local...666 == "Yes", "Failed", "Controlled"), 
-    Local.Length = as.Date(clinic.MRN$`If Local Failure, Date...671`) - scan.date, 
-    Date.Local.Failure = clinic.MRN$`If Local Failure, Date...671`,
-    
-    #Other Progression (Lobar, Nodal, Distal) Information, first Met Site
-    Lobar.Status = ifelse(clinic.MRN$Lobar...667 == "No", "Lobar.Controlled", "Lobar.Failed"),
-    Date.Lobar = clinic.MRN$`If Lobar Failure, Date`,
-    
-    Nodal.Status = ifelse(clinic.MRN$Nodal...668 == "No", "Nodal.Controlled", "Nodal.Failed"),
-    Date.Nodal = clinic.MRN$`If Nodal Failure, Date...673`,
-    
-    Distant.Status = ifelse(clinic.MRN$Distant...669 == "No", "Distant.Controlled", "Distant.Failed"),
-    Date.Distant = clinic.MRN$`If Distant Failure, Date...674`,
-    
-    #First Metastatic Site
-    First.Met = clinic.MRN$`1st Distant Metastasis Failure Site...679`,
-    
-    #Follow Up Status
-    Date.Last.Update = clinic.MRN$`Last Updated On`,
-    Date.Last.Followup = clinic.MRN$`Last Follow-up On`,
-    Date.Last.Known.Alive = clinic.MRN$`Last Known Alive`,
-    
-    Update.Time = as.Date(clinic.MRN$`Last Updated On`) - scan.date,
-    Followup.Time = as.Date(clinic.MRN$`Last Follow-up On`) - scan.date,
-    Alive.Time = as.Date(clinic.MRN$`Last Known Alive`) - scan.date
-  )
+      MRN = MRN, 
+      Scan.Date = scan.date, 
+      Sex = clinic.MRN$Gender, 
+      Ethnicity = clinic.MRN$Ethnicity, 
+      DOB = clinic.MRN$`Date of Birth`, 
+      Age.Diag = clinic.MRN$`Age at Diagnosis`, 
+      DODiag = clinic.MRN$`Date of Diagnosis...170`, 
+      Date.Rad.Start = clinic.MRN$`RT Start Date...199`,
+      
+      #Basic Patient Health Information (BMI, Years SMoking, KPS, HGB, CCI comorbidity, 
+      #Previous Cancer Diagnosis, #Number of Previous Cancer Diagnoses
+      BMI = clinic.MRN$`BMI`, 
+      Years.Smoke = clinic.MRN$`Pack Years Smoking`,
+      KPS = clinic.MRN$`KPS`, 
+      HGB = clinic.MRN$`HGB Pre`, 
+      CCI = clinic.MRN$`Total points:`,
+      Prev.Canc.Diag = clinic.MRN$`Previous Cancer Diagnosis?`,
+      Num.Prev.Canc.Diag = clinic.MRN$`Number of Previous Cancer Diagnoses`, 
+      
+      #Treatment Info; prior surgery, pre or post radiation chemo, intent of radiation, dose of radiation
+      Prior.Surg = clinic.MRN$`Prior Surgery?`, 
+      Post.Rad.Chemo = clinic.MRN$`Post-SBRT Chemo/Systemic RX?...657`,
+      Pre.Rad.Chemo = clinic.MRN$`Pre-SBRT Chemo?...191`, 
+      Rad.Intent = clinic.MRN$`Intent...194`, 
+      Tot.Dose = clinic.MRN$`Total Dose (Gy)...201`, 
+      
+      #Cancer Characteristics including tumor size, TNM, overall stage, Histology, Pathology
+      CT.Size = clinic.MRN$`CT (Cm)...177`, 
+      T.Stage = clinic.MRN$`T Stage...179`, 
+      N.Stage = clinic.MRN$`N Stage...180`,
+      M.Stage = clinic.MRN$`M Stage...181`, 
+      Overall.Stage = clinic.MRN$Stage...182, 
+      Histo = clinic.MRN$Histology...189,
+      Path = clinic.MRN$Pathology,
+      
+      #Survival Information
+      Vital.Status = clinic.MRN$`Survival Status`, 
+      OS.Length = as.Date(clinic.MRN$`Date of Death`) - scan.date,
+      Date.Death = clinic.MRN$`Date of Death`,
+      
+      #Progression Information
+      Progression.Status = ifelse(is.na(clinic.MRN$`1st Failure Date...678`), "Stable", "Progressed"), 
+      PFS.Length = as.Date(clinic.MRN$`1st Failure Date...678`) - scan.date,
+      PFS.Date = clinic.MRN$`1st Failure Date...678`,
+      PFS.Failure.Type = clinic.MRN$`Failure Type...677`,
+      
+      #Local Control Information
+      Local.Status = ifelse(clinic.MRN$Local...666 == "Yes", "Failed", "Controlled"), 
+      Local.Length = as.Date(clinic.MRN$`If Local Failure, Date...671`) - scan.date, 
+      Date.Local.Failure = clinic.MRN$`If Local Failure, Date...671`,
+      
+      #Other Progression (Lobar, Nodal, Distal) Information, first Met Site
+      Lobar.Status = ifelse(clinic.MRN$Lobar...667 == "No", "Lobar.Controlled", "Lobar.Failed"),
+      Date.Lobar = clinic.MRN$`If Lobar Failure, Date`,
+      
+      Nodal.Status = ifelse(clinic.MRN$Nodal...668 == "No", "Nodal.Controlled", "Nodal.Failed"),
+      Date.Nodal = clinic.MRN$`If Nodal Failure, Date...673`,
+      
+      Distant.Status = ifelse(clinic.MRN$Distant...669 == "No", "Distant.Controlled", "Distant.Failed"),
+      Date.Distant = clinic.MRN$`If Distant Failure, Date...674`,
+      
+      #First Metastatic Site
+      First.Met = clinic.MRN$`1st Distant Metastasis Failure Site...679`,
+      
+      #Follow Up Status
+      Date.Last.Update = clinic.MRN$`Last Updated On`,
+      Date.Last.Followup = clinic.MRN$`Last Follow-up On`,
+      Date.Last.Known.Alive = clinic.MRN$`Last Known Alive`,
+      
+      Update.Time = as.Date(clinic.MRN$`Last Updated On`) - scan.date,
+      Followup.Time = as.Date(clinic.MRN$`Last Follow-up On`) - scan.date,
+      Alive.Time = as.Date(clinic.MRN$`Last Known Alive`) - scan.date
+    )
   
-  list.all.data[[i]] <- c(list.all.data[[i]], clinic.data = list(clin.dat))
-}
+    list.all.data[[i]] <- c(list.all.data[[i]], clinic.data = list(clin.dat))
+  }
 }
 
 #Seeing How many entries dont have clinical data
@@ -215,7 +233,7 @@ for (i in 1:length(list.all.data)) {
 no.MRN.found <- which(sapply(list.all.data, length) == 4)
 
 #Removing the entries with No Clinical Data
-list.all.data <- list.all.data[-which(sapply(list.all.data, length) == 4)]
+list.all.data <- list.all.data[-no.MRN.found]
 
 #Incorporate Moments of the Distribution
 #Need Moments 1 to 4 for each Feat Curve (16 possible variables)
@@ -224,29 +242,29 @@ list.all.data <- list.all.data[-which(sapply(list.all.data, length) == 4)]
 #Skewness and Kurtosis are the standardized centralized 3rd and 4th moments respectively
 
 #Calculates raw moment
-raw.moment <- function (vec, degree) {
+raw.moment <- function(vec, degree) {
   len <- length(vec)
-  val <- sum(vec^degree)
+  val <- sum(vec ^ degree)
   
-  return(val/len)
+  return(val / len)
 }
 
 #Calculates centralized moment (by subtracting mean)
-cent.moment <- function (vec, degree) {
+cent.moment <- function(vec, degree) {
   avg <- mean(vec)
   len <- length(vec)
-  val <- sum((vec-avg)^degree)
+  val <- sum((vec - avg) ^ degree)
   
-  return(val/len)
+  return(val / len)
 }
 
 #Calculates standardized moment(by dividing sd^n)
-stand.moment <- function (vec, degree) {
+stand.moment <- function(vec, degree) {
   sd <- sd(vec)
   len <- length(vec)
-  val <- (sum((vec)^degree)/(sd^degree))
+  val <- sum(vec ^ degree) / (sd ^ degree)
   
-  return(val/len)
+  return(val / len)
 }
 
 #Calculates standardized and centralized moment
@@ -254,11 +272,11 @@ standcent.moment <- function (vec, degree) {
   avg <- mean(vec)
   sd <- sd(vec)
   len <- length(vec)
-  val <- sum(((vec-avg)/sd)^degree)
-  return(val/len)
+  val <- sum(((vec - avg) / sd) ^ degree)
+  return(val / len)
 }
 
-for (i in 1:length(list.all.data)) {
+for (i in seq_len(length(list.all.data))) {
   dim0 <- list.all.data[[i]]$feat_curves$dim0
   dim1 <- list.all.data[[i]]$feat_curves$dim1
   dim2 <- list.all.data[[i]]$feat_curves$dim2
@@ -361,8 +379,10 @@ for (i in 1:length(list.all.data)) {
                              standcent.3rd.dimtot.skew = standcent.moment(dimtot, 3), 
                              standcent.4th.dimtot.kurt = standcent.moment(dimtot, 4))
   
-  list.dimtot.mom <- list(dimtot.raw.moms = dimtot.raw.moms, dimtot.cent.moms = dimtot.cent.moms, 
-                          dimtot.stand.moms = dimtot.stand.moms, dimtot.standcent.moms = dimtot.standcent.moms)
+  list.dimtot.mom <- list(dimtot.raw.moms = dimtot.raw.moms,
+                          dimtot.cent.moms = dimtot.cent.moms, 
+                          dimtot.stand.moms = dimtot.stand.moms,
+                          dimtot.standcent.moms = dimtot.standcent.moms)
   
   #Creating a super list of all moments
   #This list -> 4 feature curves -> 4 moment flavorts -> 4 degrees (64 possible variables)
@@ -374,11 +394,13 @@ for (i in 1:length(list.all.data)) {
   
   #Putting into main list
   list.all.data[[i]] <- c(list.all.data[[i]], feat.curve.moms = list(feat.curve.moms))
-  
 }
 
 #Removing DUplicates and Keeping  Most uptodate record
-dups <- which(duplicated(sapply(list.all.data, function (x) x[["Patient.ID"]])))
+dups <- list.all.data %>%
+  sapply(FUN = function(x) x[["Patient.ID"]]) %>%
+  duplicated %>%
+  which
 list.all.data <- list.all.data[-dups]
 
 which(sapply(list.all.data, function (x) dim(x[["clinic.data"]])[1]) == 2)
@@ -400,4 +422,3 @@ list.all.data[[763]]$clinic.data <- list.all.data[[763]]$clinic.data[1, ]
 #Each list item represents a single patient
 #This object requires extracter functions to use properly
 saveRDS(list.all.data, "formatted_clinical_phom_data.rds")
-
